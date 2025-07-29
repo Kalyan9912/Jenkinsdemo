62b7cc1237909d3c6576c218cf977e5a97a13e20
Kalyan9912 <relangimavayya123@gmail.com>
2025-07-29 23:13:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-        return "Hello from Jenkins-Ansible-EC2 CI/CD Pipeline!"
+    return "Hello from Jenkins-Ansible-EC2 CI/CD Pipeline!"
 
-    if _name_ == "_main_":
-            app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
